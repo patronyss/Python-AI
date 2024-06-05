8e8326bfd21e0a684e742c50664da65c81d82da0
Olga Korneva <olyakorneva09@gmail.com>
2024-06-05 14:48:40 +0300

    task resolved: has_three_identical_digits_OlgaKornieva is added

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+def has_three_identical_digits_OlgaKornieva(num):
+    num_str = str(num)
+    digit_count = [0] * 10
+    for digit in num_str:
+        digit_count[int(digit)] ^= 1
 
-# Сюди пишіть рішення задачі (функція з вашим прізвищем)
+    for digit in set(num_str):
+        if num_str.count(digit) == 3:
+            return True
+    return False
 
-a = input("Введите начало диапозона чисел: ")
-b = input("Введите конец диапозона чисел: ")
+
+a = int(input("Введите начало диапазона чисел: "))
+b = int(input("Введите конец диапазона чисел: "))
 
 selectori = [11, 22, 33, 44, 55, 66, 77, 88, 99]
 
-for i in range(a, b+1):
-  if i - a in selectori:
-    print(i)
+for i in range(a, b + 1):
+    if has_three_identical_digits_OlgaKornieva(i):
+        print(i)
